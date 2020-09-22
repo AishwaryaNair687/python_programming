d3748742ada5199e01a723c0dfc1c81b08e46645
Aishwarya Nair <aishwariyanair68@gmail.com>
2020-09-22 08:52:55 +0000

    if,else

--- first.py
+++ first.py
@@ -1,1 +1,22 @@
 print("hello world")
+print('IN THIS PROGRAM WE WILL CREATE A PYTHON PROGRAM TO CALCUTIONS')
+print("enter 2 numbers")
+x=int(input())
+y=int(input())
+answer = 0
+print("choose any of these option (+,-,*,/) ")
+option=input()
+if option == '+':
+  answer=x+y
+elif option == '-':
+  answer=x-y
+elif option == '*':
+  answer=x*y
+elif option == '/':
+  answer=x / y
+else:
+  print("wrong option")
+
+print(x,option,y,":",answer)
+
+
